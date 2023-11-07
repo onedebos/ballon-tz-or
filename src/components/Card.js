7dbfed3667939362ec0c8d0ec2a03295920cb6a9
Adebola Adeniran <adebola@Adebolas-MacBook-Pro.local>
2023-11-07 14:03:30 +0000

    fix className in Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,11 @@
 const Card = ({ playerName, onClick }) => {
   return (
-    <div className="bg-gray-800 py-10 w-1/2 rounded-md">
+    <div className="bg-gray-800 hover:bg-gray-700 ease-in-out duration-200 py-10 w-1/2 rounded-md">
       <p className="text-center text-xl">{playerName}</p>
       <div className="flex justify-center items-center">
         <button
           onClick={onClick}
-          class="bg-violet-500 hover:bg-violet-600 px-5 py-1 rounded-full text-sm mt-3"
+          className="bg-violet-500 hover:bg-violet-600 px-5 py-1 rounded-full text-sm mt-3"
         >
           Vote Now
         </button>
