e54d62b45eff411a85a67d2e30e2a15c635ae49a
Adebola Adeniran <adebola@Adebolas-MacBook-Pro.local>
2023-11-07 15:55:25 +0000

    add getPlayers method

---
+++ src/helpers/constants.js
@@ -0,0 +1,4 @@
+const RPC_URL = "https://ghostnet.ecadinfra.com";
+const CONTRACT_ADDRESS = "KT1JV34gumzPKSHYcmZsJ11pRnmeigm5JbrH";
+
+export { RPC_URL, CONTRACT_ADDRESS };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,39 @@
 import { Inter } from "next/font/google";
 import Card from "@/components/Card";
 
+// Step 1
+import { TezosToolkit } from "@taquito/taquito";
+import { CONTRACT_ADDRESS, RPC_URL } from "@/helpers/constants";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  // Step 2
+  const Tezos = new TezosToolkit(RPC_URL);
+
+  // Step 3
+  const getPlayers = async () => {
+    try {
+      const contract = await Tezos.contract.at(CONTRACT_ADDRESS);
+      const storage = await contract.storage();
+      console.log("In the getMethods: ", storage);
+      const v = [];
+
+      storage.players.forEach((value, key) => {
+        v.push({
+          playerId: key,
+          name: value.name,
+          year: value.year,
+          votes: value.votes.toNumber(),
+        });
+      });
+
+      console.log("ss", v);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
@@ -12,7 +42,7 @@ export default function Home() {
         <h1 className="text-2xl font-bold mb-3">Ballon Tz'or 2023</h1>
       </div>
       <div className="flex flex-col items-center w-full gap-2">
-        <Card playerName={"Player Name"} />
+        <Card playerName={"Player Name"} onClick={getPlayers} />
         <Card playerName={"Player Name"} />
         <Card playerName={"Player Name"} />
         <Card playerName={"Player Name"} />
